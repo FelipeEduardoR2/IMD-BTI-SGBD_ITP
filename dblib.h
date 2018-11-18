bf045c2b16195002eed1b3bce83f720dfb9f2e48
Jão <35462554+FelipeEduardoR2@users.noreply.github.com>
2018-11-17 21:22:42 -0300

    @FelipeEduardoR2 <felipeeduardor2@gmail.com>
    Esta será a principal versão para este arquivo. E é pouco provável que seja atualizada até o final da implementação do projeto, a menos, que haja alteração na assinatura das  funções.

--- dblib.h
+++ dblib.h
@@ -1,7 +1,24 @@
 typedef struct tabela {
-	unsigned int numOfAtt;
-	char *name, **attributes, *primaryKeyName; //,*usrAdmin, *foreingKey; Ambas informações ainda serão utilizadas.
+	unsigned int numOfAtts; 
+	char *name, **attributes, *primaryKeyName;
 } table;
-int criarTabela(char *nomeDaTabela, char *nomeDoBanco, int qtdAtributos);
-void listarTabelas(char *nomeDoBanco);
-//int verificarTabela(char *tabela, char *banco); Função ainda por ser implementada.
+typedef struct linhaTabela {
+	unsigned int primaryKeyValue; 
+	char **attributes;
+} line;
+typedef struct bancoDeDados {
+	unsigned int numOfTabs;
+	char *name, *usrRoot, *pswRoot;
+} database;
+void listarBancos();
+void exibirTabela(table *tabela);
+void listarTabelas(database *banco);
+void pesquisarNaTabela(table *tabela);
+int criarBanco(database *banco);
+int autenticar(database *banco);
+int deletarTabela(table *tabela);
+int deletarBanco(database *banco);
+int inserirLinha(line *tupla, table *tabela);
+int deletarLinha(line *tupla, table *tabela);
+int criarTabela(table *tabela, database *banco);
+int verificarTabela(table *tabela, database *banco);
