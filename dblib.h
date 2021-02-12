1dad7e6bdbf692a53d1edc7ee5991de1fdfc8e2a
Felipe Eduardo <35462554+FelipeEduardoR2@users.noreply.github.com>
2021-02-12 05:21:01 -0300

    Update dblib.h

--- dblib.h
+++ dblib.h
@@ -1,24 +1,40 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//t; s;
+#define t printf("Teste!\n");
+#define s exit(EXIT_SUCCESS);
+typedef char* string;
+typedef char** vetorString;
 typedef struct tabela {
-	unsigned int numOfAtts; 
-	char *name, **attributes, *primaryKeyName;
+	unsigned int numOfAtts;
+	vetorString attributesName;
+	string name, primaryKeyName, attributesType;
 } table;
 typedef struct linhaTabela {
-	unsigned int primaryKeyValue; 
-	char **attributes;
+	vetorString attributes;
+	unsigned int primaryKeyValue;
 } line;
 typedef struct bancoDeDados {
 	unsigned int numOfTabs;
-	char *name, *usrRoot, *pswRoot;
+	string name, usrRoot, pswRoot;
 } database;
+typedef struct dadosTabela {
+	char type;
+	string value;
+} date;
 void listarBancos();
 void exibirTabela(table *tabela);
 void listarTabelas(database *banco);
-//void pesquisarNaTabela(table *tabela); Função ainda não implementada
+void pesquisarNaTabela(table *tabela);
+int validarDado(date *dado);
+int inserirLinha(table *tabela);
 int criarBanco(database *banco);
-int autenticar(database *banco);  //Função ainda não implementada
-int deletarTabela(table *tabela); //Função ainda não implementada
+int autenticar(database *banco);
+int deletarTabela(database *banco, table *tabela);
 int deletarBanco(database *banco);
-int inserirLinha(line *tupla, table *tabela);
-int deletarLinha(line *tupla, table *tabela); //Função ainda não implementada
+int deletarLinha(table *tabela);
 int criarTabela(table *tabela, database *banco);
 int verificarTabela(table *tabela, database *banco);
+int compararDados(char tipoComp, date *primeiroDado, date *segundoDado);
